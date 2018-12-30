b64f0b44b28228e4a9454babe699b5bda5d2e58c
Wahmed <wahmed@well.pk>
2018-12-30 14:12:11 +0500

    all set

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url'] = 'http://test-16685581.us-east-1.elb.amazonaws.com/';
 
 /*
 |--------------------------------------------------------------------------
